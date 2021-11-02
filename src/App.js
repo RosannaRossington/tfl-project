d4fa11a6b8beea995bfc81cab8372f3f0bcea10a
Rosanna Rossington <rosanna_rossington@outlook.com>
2021-11-02 06:40:36 +0000

    Logging TFL data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const getTFLServices = useEffect(()=>{
+    return fetch('https://api.tfl.gov.uk/Line/Mode/tube,overground,dlr/Status?detail=true').then(response => response.json()).then((data) => console.log(data))
+    },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Existing TFL services:</h1>
+     
     </div>
   );
 }
