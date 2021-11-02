1820627febcb2a9bb85fef2169f408dec8eb72bd
Rosanna Rossington <rosanna_rossington@outlook.com>
2021-11-02 09:44:04 +0000

    add service disruptions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import './App.scss';
 
 const App = () => {
   const [tflServices, setTflServices] = useState([]);
+  const [statusSeverity, setStatusSeverity] = useState(false);
+
+  const getServiceDisruption = () => {
+    return fetch(
+      'https://api.tfl.gov.uk/Line/Mode/tube,overground,dlr/Status?detail=true'
+    )
+      .then((response) => response.json())
+      .then((data)=>{
+        const statusSeverity = data.map((d)=>{
+          return d.lineStatuses.statusSeverity !== 10
+          //if there is a false in the array then there is an issue 
+        });
+        statusSeverity.includes(false) ? setStatusSeverity(false) :  setStatusSeverity(true)
+        console.log(statusSeverity, 'statusSeverity')
+        })
+  }
+
+  useEffect(()=>{
+    getServiceDisruption();
+  })
 
   useEffect(() => {
     return fetch(
@@ -23,27 +43,39 @@ const App = () => {
           return tflServices;
         });
         setTflServices(tflServiceDisplay);
-        console.log(tflServices);
+      })
+      .catch((error) => {
+        console.log('Error:', error);
       });
+
+
   }, [tflServices]);
 
   return (
     <div className='App'>
       <h1 className='title'>TFL</h1>
+      {statusSeverity && (
+        <section className='mainSection'>
+          <header>“No Service Disruptions”</header>
+        </section>
+      )}
+
       <p className='subTitle'>Existing Services</p>
       <section className='menu'>
         <header className='row'>
-          <div className='col headerTitle'>Line</div >
-          <div className='col headerTitle'>Transport Mode</div >
+          <div className='col headerTitle'>Line</div>
+          <div className='col headerTitle'>Transport Mode</div>
         </header>
         {tflServices &&
           tflServices.map((s) => {
             return (
               <div className='row'>
                 <div className='name' id={s.night}>
-                  <p className={s.delays === 'false' ? 'noDelays' : 'delays'}>{s.name}</p>
+                  <p className={s.delays === false ? 'noDelays' : 'delays'}>
+                    {s.name}
+                  </p>
                 </div>
-              
+
                 <div>
                   <p>{s.mode}</p>
                 </div>
